45d3ea486e66172384ab71e6cb322847c012e87d
sharmaeklavya <24450132+sharmaeklavya@users.noreply.github.com>
2021-06-07 17:23:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const userAuth = require("./userAuth.js");
 const port = process.env.PORT || 4000;
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+
 app.use("/", userAuth);
 
 app.listen(port);
