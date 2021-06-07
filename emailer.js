e3102bf522602a6242db2b87ad745e111629a8f5
sharmaeklavya <24450132+sharmaeklavya@users.noreply.github.com>
2021-06-08 00:10:03 +0530

    Update emailer.js

--- emailer.js
+++ emailer.js
@@ -17,9 +17,9 @@ const auth = async (emailaddress) => {
       from: '"SpotPass 😊" <errik41@gmail.com>',
       to: emailaddress,
       subject: "Password Reset",
-      html: `<div style="position:absolute; top:50%; left:50%; transform: translate(-50%, -50%); border: 1px solid lightgray; border-radius: 5px; padding: 1rem;">
-      <h1 style="font-size:1.2rem; color:salmon;">Verify this email to reset your password</h1>
-      <div style="padding: 1rem; margin: 0.75rem 0; text-align: center;">
+      html: `<div style="position:absolute; top:50%; left:50%; transform: translate(-50%, -50%); border: 1px solid lightgray; border-radius: 5px; padding: 1rem; text-align:center;">
+      <h1 style="font-size:1.2rem; color:salmon;">Dear User, If you have requested for a new password. Please verify this email to reset your password or simply ignore this email.</h1>
+      <div style="padding: 1rem; margin: 0.75rem 0;">
         <img style="width:300px; height:350px;" src="https://images.unsplash.com/photo-1582161990632-b63fc933dd93?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" style="width:100%; height:100%;" alt="recover-password">
       </div>
       <a href="http://localhost:3000/${verificationString}" style="font-size: 1.2rem; padding:0.75rem; border:none; border-radius:5px; text-decoration:none; background-color: rgb(76, 175, 75); color: whitesmoke; cursor:pointer;">Reset Now</a>
