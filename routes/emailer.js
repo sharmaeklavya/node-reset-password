cb4c0d9ba2e39cd0995bd566d32372453c44d1a1
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-06-21 11:00:25 +0530

    Update emailer.js

--- routes/emailer.js
+++ routes/emailer.js
@@ -30,7 +30,7 @@ const auth = async (emailaddress) => {
         <img src="https://images.unsplash.com/photo-1590280986931-d675a2cf9b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1047&q=80" style="width:100%; height:100%; text-decoration:none; cursor: pointer;" alt="recover-password">
       </a>
         </div>
-      <a href="https://proj-resetpassword.netlify.app/reset/${verificationString}" style="font-size: 1rem; padding:0.75rem; border:none; border-radius:5px; text-decoration:none; background-color: rgb(76, 175, 75); color: whitesmoke; cursor:pointer;">Reset Now</a>
+      <a href="https://proj-reset-password.netlify.app/reset-password/${verificationString}" style="font-size: 1rem; padding:0.75rem; border:none; border-radius:5px; text-decoration:none; background-color: rgb(76, 175, 75); color: whitesmoke; cursor:pointer;">Reset Now</a>
       </div>`,
     });
   } catch (error) {
