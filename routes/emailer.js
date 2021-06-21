e5c10a93d206301c0eb33a54bbe7263d92cb2b7d
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-06-21 11:02:51 +0530

    Update emailer.js

--- routes/emailer.js
+++ routes/emailer.js
@@ -26,7 +26,7 @@ const auth = async (emailaddress) => {
       html: `<div style="margin : 0 auto; width: 450px; border:1px solid lightgray; border-radius:5px; padding:1rem; text-align:center;">
       <h1 style="font-size:1rem; color:salmon; text-align: left;"> <lead>Dear user,</lead> <br/> <br/>if you have requested for a new password. Please verify this email to reset the password or simply ignore this email.</h1>
       <div style="padding:1rem; margin:0.75rem auto; width:400px; height: 300px;">
-      <a href="https://proj-resetpassword.netlify.app/reset/${verificationString}">
+      <a href="https://proj-reset-password.netlify.app/reset-password/${verificationString}">
         <img src="https://images.unsplash.com/photo-1590280986931-d675a2cf9b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1047&q=80" style="width:100%; height:100%; text-decoration:none; cursor: pointer;" alt="recover-password">
       </a>
         </div>
